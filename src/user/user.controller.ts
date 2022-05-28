eb48d84c40f9eacdb1c140cd90f4d1ea2ab01cff
jinho1011 <gino9940@gmail.com>
2022-05-29 01:08:49 +0900

    Fix user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -4,10 +4,14 @@ import {
   Delete,
   Get,
   Param,
+  ParseUUIDPipe,
   Patch,
   Post,
   Query,
 } from '@nestjs/common';
+import { User } from 'src/entity/user.entity';
+import { CreateUserDto } from './dto/createUser.dto';
+import { UpdateUserDto } from './dto/updateUser.dto';
 import { UserService } from './user.service';
 
 @Controller('user')
@@ -15,32 +19,35 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get('/all')
-  getAllUser() {
+  getAllUser(): Promise<User[]> {
     return this.userService.getAllUser();
   }
 
   @Get('/:id')
-  getUserByParam(@Param('id') id: number) {
+  getUserByParam(@Param('id', ParseUUIDPipe) id: string): Promise<User> {
     return this.userService.getUser(id);
   }
 
   @Get()
-  getUserByQuery(@Query('id') id: number) {
+  getUserByQuery(@Query('id', ParseUUIDPipe) id: string): Promise<User> {
     return this.userService.getUser(id);
   }
 
   @Post('/create')
-  createUser(@Body() userData) {
-    return this.userService.createUser(userData);
+  createUser(@Body() createUserDto: CreateUserDto): Promise<boolean> {
+    return this.userService.createUser(createUserDto);
   }
 
   @Patch(':id')
-  updateUser(@Param('id') id: number, @Body() userData) {
-    return this.userService.updateUser(id, userData);
+  updateUser(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ): Promise<boolean> {
+    return this.userService.updateUser(id, updateUserDto);
   }
 
   @Delete(':id')
-  deleteUser(@Param('id') id: number) {
+  deleteUser(@Param('id', ParseUUIDPipe) id: string): Promise<boolean> {
     return this.userService.deleteUser(id);
   }
 }
