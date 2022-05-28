be8f0c836bbbdf0d8d39ee8030121b724ab57a2a
jinho1011 <gino9940@gmail.com>
2022-05-29 00:56:49 +0900

    Define user entity

--- src/entity/user.entity.ts
+++ src/entity/user.entity.ts
@@ -1,1 +1,41 @@
-export class User {}
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  Unique,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'user' })
+@Unique(['userId'])
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: 50 })
+  userId: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  password: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  salt: string;
+
+  @Column({ type: 'varchar', length: 30 })
+  name: string;
+
+  @Column({ type: 'tinyint' })
+  age: number;
+
+  @CreateDateColumn({ name: 'createdAt' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updatedAt' })
+  upadtedAt: Date;
+
+  @DeleteDateColumn({ name: 'deletedAt' })
+  deletedAt?: Date | null;
+}
