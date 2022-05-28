b48af24f4536b5ff37f2560602ed5187c51348d3
jinho1011 <gino9940@gmail.com>
2022-05-28 15:29:55 +0900

    Add REST methods

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get('/all')
+  getAllUser() {
+    return this.userService.getAllUser();
+  }
+
+  @Get('/:id')
+  getUserByParam(@Param('id') id: number) {
+    return this.userService.getUser(id);
+  }
+
+  @Get()
+  getUserByQuery(@Query('id') id: number) {
+    return this.userService.getUser(id);
+  }
+
+  @Post('/create')
+  createUser(@Body() userData) {
+    return this.userService.createUser(userData);
+  }
+
+  @Patch(':id')
+  updateUser(@Param('id') id: number, @Body() userData) {
+    return this.userService.updateUser(id, userData);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id: number) {
+    return this.userService.deleteUser(id);
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  private users = [];
+
+  deleteUser(id: number) {
+    throw new Error('Method not implemented.');
+  }
+  updateUser(id: number, userData: any) {
+    throw new Error('Method not implemented.');
+  }
+  createUser(userData: any) {
+    throw new Error('Method not implemented.');
+  }
+  getUser(id: number) {
+    throw new Error('Method not implemented.');
+  }
+  getAllUser() {
+    throw new Error('Method not implemented.');
+  }
+}
