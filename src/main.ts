7bd32ba411aa732d3c6b2faaa5d34ff26a6eb958
jinho1011 <gino9940@gmail.com>
2022-05-28 18:27:30 +0900

    Add ValidationPipe option description

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
+      // whitelist : DTO에 없는 속성 제외
       whitelist: true,
+      // forbidNonWhitelisted : 전달하는 요청 값 중에 정의 되지 않은 값이 있으면 Error 발생
       forbidNonWhitelisted: true,
+      // transform : 객체를 자동으로 DTO로 변환을 원하면 transform 값을 true로 설정
       transform: true,
     }),
   );
