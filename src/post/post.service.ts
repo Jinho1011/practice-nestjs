2496e49e89d13aa5cba652371cd6d24462034e32
jinho1011 <gino9940@gmail.com>
2022-06-04 03:15:22 +0900

    Implement post service

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,17 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { CreatePostDto } from './dto/create-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
+import { PostRepository } from './post.repository';
 
 @Injectable()
 export class PostService {
-  deletePost(id: string) {
-    throw new Error('Method not implemented.');
-  }
-  updatePost(id: string) {
-    throw new Error('Method not implemented.');
-  }
-  createPost() {
-    throw new Error('Method not implemented.');
-  }
+  constructor(
+    @InjectRepository(PostRepository) private postRepository: PostRepository,
+  ) {}
+
   getPostById(id: string) {
-    throw new Error('Method not implemented.');
+    return this.postRepository.findById(id);
+  }
+
+  createPost(createPostDto: CreatePostDto) {
+    return this.postRepository.onCreate(createPostDto);
+  }
+
+  deletePost(id: string) {
+    return this.postRepository.onDelete(id);
+  }
+
+  updatePost(id: string, updatePostDto: UpdatePostDto) {
+    return this.postRepository.onUpdate(id, updatePostDto);
   }
 }
